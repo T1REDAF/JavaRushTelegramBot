39de416cb20049e42f87b96bec469eecd5c48fef
T1REDAF <88323810+T1REDAF@users.noreply.github.com>
2022-02-01 15:54:57 +0500

    Update V00001__add_tg_user_table.sql

--- src/main/resources/db/migration/V00001__add_tg_user_table.sql
+++ src/main/resources/db/migration/V00001__add_tg_user_table.sql
@@ -1,8 +1,8 @@
 -- ensure that the table with this name is removed before creating a new one.
-DROP TABLE IF EXISTS tg_user;
+DROP TABLE IF EXISTS tg_user1;
 
 -- Create tg_user table
-CREATE TABLE tg_user (
+CREATE TABLE tg_user1 (
                          chat_id VARCHAR(100),
                          active BOOLEAN
 );
